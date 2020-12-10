370550c1203ce887849992420093398b14aeb83f
longnh18 <v.longnh18@vinid.net>
2020-12-10 17:51:53 +0700

    update

--- src/files/__path@dasherize__/__name@dasherize__-routing.module.ts
+++ src/files/__path@dasherize__/__name@dasherize__-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from '@common/services';
-import { appRoutes } from './routes';
 
 const routes: Routes = [];
 
